e01995d99638146aecad105443d5387ff7e118f1
kipronorogers <kipronorogers@gmail.com>
2021-03-10 15:46:50 +0300

    bootstrap

---
+++ bootstrap/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $("#design-image").click(function(){
+    $("#design-image").hide();
+    $("#design").show();
+  });
+  $("#design").click(function(){
+    $("#design").hide();
+    $("#design-image").show();
+  });
+});
+
+$(document).ready(function(){
+  $("#development-image").click(function(){
+    $("#development-image").hide();
+    $("#development").show();
+  });
+  
+});$("#development").click(function(){
+  $("#development").hide();
+  $("#development-image").show();
+});
+
+$(document).ready(function(){
+  $("#product-image").click(function(){
+    $("#product-image").hide();
+    $("#product").show();
+  });
+  $("#product").click(function(){
+    $("#product").hide();
+    $("#product-image").show();
+  });
+});
+
+
